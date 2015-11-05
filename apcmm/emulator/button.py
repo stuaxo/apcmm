875759c2b3aadd0677a2e43b617adff2a7e539fc
Stuart Axon <stuaxo2@yahoo.com>
2015-11-06 03:42:16 +0800

    Remove more test/democode

--- apcmm/emulator/button.py
+++ apcmm/emulator/button.py
@@ -2,8 +2,6 @@ from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 
-import apcmm.api.model as model
-
 
 class ClipButton(ButtonBehavior, BoxLayout):
     def __init__(self, data):
@@ -35,33 +33,19 @@ class RoundButton(ButtonBehavior, BoxLayout):
         """
         self.children[0].source = "images/round-button-%s.png" % data.light_color.name
 
+
 class ControlButton(RoundButton):
     def __init__(self, data):
         RoundButton.__init__(self, data)
-
-    def on_press(self):
-        print self.data.set_color(model.ControlColors.red)
-
-    def on_release(self):
-        print self.data.light_color
-
 
 
 class SceneButton(RoundButton):
     def __init__(self, data):
         RoundButton.__init__(self, data)
 
-    def on_press(self):
-        print self.data.set_color(model.SceneColors.green)
-
-    def on_release(self):
-        print self.data.light_color
-
 
 class ShiftButton(Button):
     def __init__(self, data):
         self.widget_data = data
         Button.__init__(self, id=str(data.name), text="shift")
-
-
 
